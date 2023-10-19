247afeecb5670736b6cb3a14c39e383b1ef2fa0b
Vaughn Wilcox <vaughn_wilcox@yahoo.com>
2023-10-18 21:01:03 -0400

    Iterations cont.

--- Unit_3/Arrays/iterationMethods.js
+++ Unit_3/Arrays/iterationMethods.js
@@ -44,3 +44,122 @@ const noFives = myNumberArray.filter(fiveLess => {
 })
 
 console.log(noFives);
+
+const unordered = [1,5,2,7,9];
+
+unordered.sort(function(a, b){
+    return a-b
+});
+console.log(unordered);
+
+
+
+let moves = [
+    { name: "Top Gun 2", category: 'Action' },
+    { name: "Scary Movie 5", category: 'Horror' },
+];
+
+movies.push({ name: "It", category: 'Horror' });
+movies.push({ name: "Get Out", category: 'Horror' });
+movies.push({ name: "Taken", category: 'Action' });
+
+const actionMovies = movies.filter(movie => movie.category === 'Action' );
+const horrorMovies = movies.filter(movie => movie.category === 'Horror' );
+
+console.log('Horror', horrorMovies);
+console.log('Action', actionMovies);
+
+//! .forEach()
+
+let newFoodList = [
+    'apple', 'pear','mushroom','cheese','peach'
+];
+
+//for(let i = 0; i < newFoodList.length; i++) {
+//    console.log(newFoodList[i])
+//}
+
+newFoodList.forEach((item, index) => {
+    console.log('ForEach: ', item, "index", index);
+});
+
+/*
+    - (Go look at MDN docs to remind you):
+    - Create an array containing movies titles
+    - Use .forEach() to list your movies
+    - Add another movie at the end
+    - And replace one of your existing movies with another one
+*/
+
+let movies = ["Matrix", "Babe", "Little Mermaid", "The Two Towers"]
+    movies.forEach((item, index) => {console.log( item, index );
+});
+
+movies.push('Aladdin');
+console.log("Push:", movies);
+
+movies.splice(1, 1, "Universal Soldier");
+
+console.log(movies);
+
+//! .find()
+let tmnt= [
+    'Mikey', 'Donnie', 'Leo', 'Raph', 'Splinter', 'Shredder', 'Baxter'
+];
+
+let character = 'Leo';
+console.log('Find', tmnt.find(c => c == character));
+
+character = 'April';
+console.log('Find Again:', tmnt.find(c => c == character));
+
+character = 'Splinter';
+tmnt.find((c,i) => console.log('Index: ', c == character, "Index" , i));
+
+//! .map()
+/*
+    - Similar to .forEach()
+    - takes a callback function (cb)
+*/
+
+let numArray = [];
+let fizzBuzzArray = [];
+
+for(let i = 0; i < 101; i++) numArray.push(i);
+
+numArray.map(x => {
+    if(x % 15 === 0) fizzBuzzArray.push(x);
+});
+
+console.log(fizzBuzzArray);
+
+/*
+- First check if you are working with an array
+    - Using a method, flip the values within the array 
+    (what was in index 4 is now in 0, 3 to 1, etc.)
+    - Using a method, print the values of the newly arranged array
+    
+    hint: 
+    - search something like "arrays", "javascript", "reverse"
+    - look in your notes to see how we can check if something is an array
+*/
+
+let arr1 = [1,2,3,4,5];
+if(arr1 instanceof Array === true)
+        arr1.sort(function(a, b){
+        return b-a
+});
+
+
+console.log(arr1);
+
+
+
+let arr = [1,2,3,4,5];
+
+if(arr instanceof Array === true) {
+    let revArr =  arr.reverse();
+    revArr.forEach(num => console.log(num));
+} else {
+    console.log('not an array');
+}
