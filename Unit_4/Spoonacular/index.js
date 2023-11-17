f2ac059fa83a349e6d26a86d6742402f60c04b3e
Vaughn Wilcox <vaughn_wilcox@yahoo.com>
2023-11-16 19:55:25 -0500

    Spoontacular fininshing

--- Unit_4/Spoonacular/index.js
+++ Unit_4/Spoonacular/index.js
@@ -5,18 +5,30 @@ import apiKey from "./key.js";
 const baseURL = 'https://api.spoonacular.com/recipes/random';
 let buildURL = `${baseURL}/?apiKey=${apiKey}`;
 let storedRecipes = [];
+
+
 //! DOM Elements
 const searchForm = document.querySelector('form');
 const randomCard = document.querySelector('.random-card');
-
+const keptCards = document.getElementById('kept-cards')
 
 //! Functions
-//TODO Display Singles card
+const removeElements = element => {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+//* TODO Display Singles card
+
+
+
 const displayRandomCard = recipe => {
     //console.log(recipe);
-    while(randomCard.firstChild) { //Clears the previously created card so it doesn't display a second.
-        randomCard.removeChild(randomCard.firstChild);
-    }
+    //while(randomCard.firstChild) { 
+        //Clears the previously created card so it doesn't display a second.
+    //randomCard.removeChild(randomCard.firstChild);
+    removeElements(randomCard);
+    
 
     //* Create Elements
     let card = document.createElement('div');
@@ -29,7 +41,7 @@ const displayRandomCard = recipe => {
     card.className = 'card';
     card.style.width = '18rem';
     img.src = recipe.img;
-    img.className = 'card-img-top'
+    img.className = 'card-img-top';
     img.alt = recipe.title;
     body.className = 'card-body';
     title.className = 'card-title';
@@ -37,10 +49,10 @@ const displayRandomCard = recipe => {
     btn.className = 'btn btn-primary';
     btn.textContent = 'Add';
     btn.onclick = () => {
-        storedRecipes.push(recipe);
+        storedRecipes.unshift(recipe);
         setTable();
 
-    }
+    };
 
     //* Attach Elements
     body.appendChild(title);
@@ -52,10 +64,51 @@ const displayRandomCard = recipe => {
     randomCard.appendChild(card);
 
 
-}
+};
 //TODO Display Deck of Cards
 const setTable = () => {
     console.log('Stored Recipe Array: ', storedRecipes);
+
+    removeElements(keptCards);
+    storedRecipes.map(obj => {
+
+    
+    //*Create 
+    let div = document.createElement('div');
+    let card = document.createElement('div');
+    let img = document.createElement('img');
+    let body = document.createElement('div');
+    let title = document.createElement('h5');
+    let p = document.createElement('p');
+    let a = document.createElement('a');
+
+
+    //* Set Attributes
+    div.className = 'col';
+    card.className = 'card';
+    img.src = obj.img; // Needs an object from the "storedRecipes" array
+    img.alt = obj.title; // Needs an object from the "storedRecipes" array 
+    //img.className = 'card-img-top';
+    body.className = 'card-body';
+    title.className = 'card-title';
+    title.textContent = obj.title // Needs an object from the "storedRecipes" array 
+    p.className = 'card-text';
+    a.href = obj.src;// Needs an object from the "storedRecipes" array 
+    a.target = '_blank'; //Moves to a different page
+    a.textContent = 'Link to Recipe';
+    
+    
+    //* Append to parent
+    p.appendChild(a);
+    body.appendChild(title);
+    body.appendChild(p);
+    card.appendChild(img);
+    card.appendChild(body);
+
+    div.appendChild(card);
+    keptCards.appendChild(div);
+});
+
 }
 //! Event Listeners
 searchForm.addEventListener('submit', e=> {
