51dc9f25246e5ce45d489c7230cacb0c0505703e
Vaughn Wilcox <vaughn_wilcox@yahoo.com>
2023-12-05 18:57:46 -0500

    Novels challenge

--- Unit_5/Challenges/newExpressApp/controllers/books.controller.js
+++ Unit_5/Challenges/newExpressApp/controllers/books.controller.js
@@ -1,66 +1,67 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const db = require(`../data.json`);
 
+router.get("/", (req, res) => {
+  //Request all
+  try {
+    res.status(200).json({
+      results: db,
+    });
+    console.log(db);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
 
+router.get("/find/:id", (req, res) => {
+  //Request by ID
 
-router.get('/', (req, res) => { //Request all
-    try {
+  try {
+    let { id } = req.params;
+    let results = db.filter((i) => i.id == id);
 
+    console.log(results);
+
+    res.status(200).json({
+      results: results[0],
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
+
+router.get("/query", (req, res) => {
+  //Query by title
+
+  try {
+    if (title) {
+      const { title } = req.query;
+      if (title) {
+        console.log(title);
         res.status(200).json({
-            results:db
-            
-        })
-        console.log(db);
-    } catch (err) {
-        res.status(500).json({
-            error: err.message
-        })
+          results: results[0].title,
+        });
+      } else {
+        throw new Error("Unable to find this novel.");
+      }
     }
-})
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
 
+router.post("/", (req, res) => {
+  console.log(req.body);
 
-router.get('/find/:id',(req, res) => { //Request by ID
-    
+  const { title, author } = req.body;
 
-    
-
-    try {
-        
-        let { id } = req.params;
-        let results = db.filter(i => i.id == id);
-
-        console.log(results);
-
-        res.status(200).json({
-            results: results[0]
-        })
-
-    } catch (err) {
-        res.status(500).json({
-            error: err.message
-        })
-    }
-})
-
-router.get('/query', (req,res) => { //Query by title
-    
-    try { 
-        if (title){
-        const {title} = req.query;    
-        if (title) {
-            console.log(title)
-                res.status(200).json({
-                    results: results[0].title
-                                    })
-                    } else {
-                        throw new Error("Unable to find this novel.")
-                    }
-} 
-    } catch (err) {
-        res.status(500).json({
-            error: err.message
-        }) 
-    }
-})
+  res.status(200).json({ title, author });
+});
 
 module.exports = router;
